40e053c89ee5e03b4006a9d892eef1a3ece1750e
Jeff Tian@Workstation <jie.tian@live.cn>
2022-01-31 18:20:33 +0800

    feat: for eval5

--- scripts/wmp.js
+++ scripts/wmp.js
@@ -9,6 +9,7 @@ const input = process.argv[2] || 'index.js'
 
 const file = path.join(process.cwd(), input)
 const convertedFile = path.join(process.cwd(), input + '.converted')
+const eval5File = path.join(process.cwd(), input + '.eval5')
 
 console.log('handling ', file)
 
@@ -20,4 +21,7 @@ const text = fs.readFileSync(file, 'utf-8')
 
 const converted = text.replace(/WebAssembly/g, 'WXWebAssembly').replace('self.location.href', '(self || this).location.href').replace(/return getBinaryPromise\(\)\.then\(.+?\}\)/s, `return WXWebAssembly.instantiate(this.wasmPath, info)`).replace('instantiateAsync();', 'instantiateArrayBuffer(receiveInstantiationResult);').replace(`var Module = typeof Module !== 'undefined' ? Module : {};`, `var Module = typeof this.Module !== 'undefined' ? this.Module : {};`);
 
+const eval5 = text.replace(/WebAssembly/g, 'WXWebAssembly').replace('self.location.href', '(self || global).location.href').replace(/return getBinaryPromise\(\)\.then\(.+?\}\)/s, `return WXWebAssembly.instantiate(global.wasmPath, info)`).replace('instantiateAsync();', 'instantiateArrayBuffer(receiveInstantiationResult);').replace(`var Module = typeof Module !== 'undefined' ? Module : {};`, `var Module = typeof global.Module !== 'undefined' ? global.Module : {};`);
+
 fs.writeFileSync(convertedFile, converted);
+fs.writeFileSync(eval5File, eval5);
