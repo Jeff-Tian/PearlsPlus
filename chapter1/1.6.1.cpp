2ee09a8fae8f1787891cb31518cef15577ff8daa
Jeff Tian <jeff.tian@outlook.com>
2022-02-05 08:43:36 +0000

    feat: 1.6.1.c

--- chapter1/1.6.1.cpp
+++ chapter1/1.6.1.cpp
@@ -10,6 +10,8 @@ int main(void) {
     int i;
     set<int>::iterator j;
 
+    cout << "1. 如果不缺内存，如何使用一个具有库的语言来实现一种排序算法以表示和排序集合？\n";
+
     cout << "请输入要排序的数字集合，一行一个。输入完成后，回车后按 Ctrl+D 结束输入：\n";
 
     while(cin >> i) {
