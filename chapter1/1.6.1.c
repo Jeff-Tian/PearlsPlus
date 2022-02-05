3046c49130fb7a11731efb9775beaacc3f2cd8c1
Jeff-Tian <Jeff-Tian@users.noreply.github.com>
2022-02-05 08:44:50 +0000

    deploy: 52649feff408883e011bdc55fac8a40f8988ae94

---
+++ chapter1/1.6.1.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+
+int intcomp(int *x, int *y) {
+    return *x - *y;
+}
+
+int a[1000000];
+int main(void) {
+    int i, n = 0;
+
+    printf("1. 如果不缺内存，如何使用一个具有库的语言来实现一种排序算法以表示和排序集合？\n\n");
+
+    printf("请输入整数，一行一个。按 Ctrl+D 结束输入：\n\n");
+    while (scanf("%d", &a[n]) != EOF) {
+        n++;
+    }
+
+    qsort(a, n, sizeof(int), intcomp);
+
+    printf("排序后的结果是：\n\n");
+    
+    for(i = 0; i < n; i++) {
+        printf("%d\n", a[i]);
+    }
+
+    return 0;
+}
